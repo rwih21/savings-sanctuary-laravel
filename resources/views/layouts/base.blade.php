b61d9d990428ec78ecff3c5c6533c08df211fd7a
rwih21 <robbyhtb.personal@gmail.com>
2026-04-27 16:27:12 +0700

    style test

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Savings Sanctuary')</title>
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/main.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}?v={{ filemtime(public_path('css/main.css')) }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
